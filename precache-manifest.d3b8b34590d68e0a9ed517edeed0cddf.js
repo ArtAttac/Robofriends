56d55e721f819a80e3f658802c72da7433857bbd
Art I <sovietbulldozer@gmail.com>
2019-11-20 12:22:20 -0500

    Updates

---
+++ precache-manifest.d3b8b34590d68e0a9ed517edeed0cddf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4767467d0f9ca0905961a314864ab52a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "d5ccf6a0d7a6e000199d",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "877384cc0710aba129ae",
+    "url": "/robofriends/static/css/main.7450b351.chunk.css"
+  },
+  {
+    "revision": "d5ccf6a0d7a6e000199d",
+    "url": "/robofriends/static/js/2.69e24287.chunk.js"
+  },
+  {
+    "revision": "877384cc0710aba129ae",
+    "url": "/robofriends/static/js/main.15926392.chunk.js"
+  },
+  {
+    "revision": "37773780271047ce06e4",
+    "url": "/robofriends/static/js/runtime-main.55723ab0.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
